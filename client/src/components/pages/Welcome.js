1902363350dcaba0a0341d5cdf9074c276730adc
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-10-27 11:28:18 -0700

    requireauth, welcome cleanup

--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -1,3 +1,17 @@
 export default function Welcome() {
-  return <h1>Welcome to the Berry Club 🍇</h1>;
+  return (
+    <div>
+      <div className="ui dividing header">
+        <h1>Welcome to the Berry Club 🍇</h1>
+      </div>
+      <div className="ui content">
+        <h3>Features</h3>
+        <ul>
+          <li>Track you recipes</li>
+          <li>Add recipe ingredients to you grocery list</li>
+          <li>View friends' recipes</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- client/src/components/requireAuth.js
+++ client/src/components/requireAuth.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function requireAuth(ChildComponent) {
   return function ComposedComponent(props) {
     //navigates back to the root page if auth is false
     const auth = useSelector((state) => state.auth);
-    console.log(auth);
+    const { pathname } = useLocation();
 
     if (auth === false) {
-      console.log("RrquireAuth HOC blocked navigation");
+      console.log("requireAuth HOC blocked navigation to", pathname);
       return <Navigate to="/" />;
     }
 
