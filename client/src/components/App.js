73d924f308a56815dad0420e8efc3f86521335e6
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-10-27 18:20:26 -0700

    app converted to function component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { connect } from "react-redux";
 
 import Nav from "./Nav";
 import Welcome from "./pages/Welcome";
 import Dashboard from "./pages/Dashboard";
 import Recipes from "./pages/Recipes";
 
-import { getCurrentUser } from "../actions";
+import { GET_CURRENT_USER } from "../actions/types";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //see if user is logged in with google already
-    this.props.getCurrentUser();
-  }
+function App() {
+  const dispatch = useDispatch();
 
-  render() {
-    return (
-      <div style={{ height: "100vh", background: "darkslateblue" }}>
-        <Nav />
-        <div className="ui container">
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/recipes" element={<Recipes />} />
-          </Routes>
-        </div>
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      //see if user is logged in with google already
+      const res = await axios.get("/auth/current_user");
+      dispatch({ type: GET_CURRENT_USER, payload: res.data });
+    };
+    getCurrentUser();
+  });
+
+  return (
+    <div style={{ height: "100vh", background: "darkslateblue" }}>
+      <Nav />
+      <div className="ui container">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/recipes" element={<Recipes />} />
+        </Routes>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default connect(null, { getCurrentUser })(App);
+export default App;
